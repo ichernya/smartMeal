73406c4223f349619662c6a97464e9b1c9133839
Ivan Chernyak <ivanchernyak@Ivans-MacBook-Pro-2.local>
2023-01-23 11:38:26 -0800

    backend added, docker compose up, swaggerUI runs port 3010

---
+++ backend/src/app.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const cors = require('cors');
+const yaml = require('js-yaml');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
+const OpenApiValidator = require('express-openapi-validator');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const apiSpec = path.join(__dirname, '../api/openapi.yaml');
+
+const apidoc = yaml.load(fs.readFileSync(apiSpec, 'utf8'));
+app.use('/v0/api-docs', swaggerUi.serve, swaggerUi.setup(apidoc));
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: apiSpec,
+      validateRequests: true,
+      validateResponses: true,
+    }),
+);
+  
+  module.exports = app;
