12501bb758edf41f53775d94bb3a679028541804
Ivan Chernyak <ivanchernyak@Ivans-MacBook-Pro-2.local>
2023-01-24 12:53:10 -0800

    small changes to schema

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE recipes(
+    recipeid int NOT NULL AUTO_INCREMENT,
     dishname VARCHAR(32),
     ingredients text[]
     );
@@ -11,7 +12,26 @@ CREATE TABLE users(
     userid int NOT NULL AUTO_INCREMENT,
     username VARCHAR(32) NOT NULL,
     passwrd VARCHAR(32) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (userid),
     );
+
+DROP TABLE IF EXISTS meals;
+
+CREATE TABLE meals(
+    mealsid int,
+    recipeid int,
+    day DATETIME,
+    FOREIGN KEY meals(recipeid) REFERENCES recipes(recipeid),
+    FOREIGN KEY meals(mealsid) REFERENCES user(userid)
+)
+
+/*
+Every user has an id used to match meals object with same id
+Every meal has an id used to match recipe for that meal
+
+to find what someone has on that day of eating:
+query for userid date and recipeid,
+and you will recieve a meal objects where all 3 are true.
+*/
 
 
