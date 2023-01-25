e8b7fb162d8cf55a6f8d4438f11efdc95d06de72
Edmond Poon <edpoon02@gmail.com>
2023-01-25 12:30:55 -0800

    update routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-
-// eslint-disable-next-line no-unused-vars
 import {BrowserRouter, Route, Routes, Navigate, Outlet} from 'react-router-dom';
 import DimensionsProvider from './components/dimensions.jsx';
 import Homepage from './components/Home.jsx';
-import './App.css';
-// import Login from './components/Login.jsx';
+import Login from './components/Login.jsx';
 import Registration from './components/Registration.jsx';
-// eslint-disable-next-line require-jsdoc
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Registration/>
       <DimensionsProvider>
         <BrowserRouter>
           <Routes>
             <Route path={'/week'} exact element={<Homepage/>}/>
+            <Route path={'/signup'} exact element={<Registration/>}/>
+            <Route path={'/login'} exact element={<Login/>}/>
           </Routes>
         </BrowserRouter>
       </DimensionsProvider>
     </div>
   );
 }
+
 export default App;
