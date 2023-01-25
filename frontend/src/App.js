3a4687f4139a2a4a6ed707e5716fd2c090f2dc84
Edmond Poon <edpoon02@gmail.com>
2023-01-25 12:06:30 -0800

    added list of foods that are on the menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes, Navigate, Outlet} from 'react-router-dom';
 import DimensionsProvider from './components/dimensions.jsx';
 import Homepage from './components/Home.jsx';
+import './App.css';
 
 function App() {
   return (
     <div>
-        <DimensionsProvider>
-            <div className="App">
-                <Homepage/>
-            </div>
-        </DimensionsProvider>
+      <DimensionsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'/week'} exact element={<Homepage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </DimensionsProvider>
     </div>
   );
 }
