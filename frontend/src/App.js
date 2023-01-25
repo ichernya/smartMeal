dfd454dff8890948b5d859c8c4dcb4f2ba3f426f
Eric <meisericxie@gmail.com>
2023-01-25 12:29:32 -0800

    Bug fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,18 @@ import {BrowserRouter, Route, Routes, Navigate, Outlet} from 'react-router-dom';
 import DimensionsProvider from './components/dimensions.jsx';
 import Homepage from './components/Home.jsx';
 import './App.css';
-// import Login from './components/Login.jsx';
+import Login from './components/Login.jsx';
 import Registration from './components/Registration.jsx';
 // eslint-disable-next-line require-jsdoc
 function App() {
   return (
     <div className="App">
-      <Registration/>
       <DimensionsProvider>
         <BrowserRouter>
           <Routes>
             <Route path={'/week'} exact element={<Homepage/>}/>
+            <Route path={'/login'} exact element={<Login/>}/>
+            <Route path={'/signup'} exact element={<Registration/>}/>
           </Routes>
         </BrowserRouter>
       </DimensionsProvider>
