506b7934dd023f906fc5cda61f09482380022556
Eric <meisericxie@gmail.com>
2023-02-01 13:08:19 -0800

    Stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,9 @@ import DimensionsProvider from './components/DimensionsProvider.jsx';
 import Homepage from './components/Homepage/Home.jsx';
 import Login from './components/Login_Registration/Login.jsx';
 import Registration from './components/Login_Registration/Registration.jsx';
+import Error from './components/Page404.jsx';
+import PageLayout from './components/PageLayout.jsx';
+import SideBar from './components/SideBar.jsx';
 import './App.css';
 
 // eslint-disable-next-line require-jsdoc
@@ -15,9 +18,13 @@ function App() {
           <Routes>
             <Route element={<RequireAuth/>}>
               <Route path={'/week'} exact element={<Homepage/>}/>
+              <Route path={'/home'} exact element={<PageLayout/>}/>
+
             </Route>
             <Route path={'/signup'} exact element={<Registration/>}/>
             <Route path={'/login'} exact element={<Login/>}/>
+            <Route path={'/sidebar'} exact element={<SideBar/>}/>
+            <Route path={'*'} exact element={<Error/>}/>
           </Routes>
         </BrowserRouter>
       </DimensionsProvider>
