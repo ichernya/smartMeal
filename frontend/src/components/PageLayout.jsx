d5e47dad02ee740bbf111b2d039bbadbb2049094
Eric <meisericxie@gmail.com>
2023-02-03 11:29:42 -0800

    Sidebar icon

--- frontend/src/components/PageLayout.jsx
+++ frontend/src/components/PageLayout.jsx
@@ -8,15 +8,14 @@ import './SideBar.css';
 
 const page = () => {
   return (
-        <Grid container spacing={0}>
-          <Grid item xs={1}>
-            <SideBar className='SideBar'/>
-          </Grid>
-          <Grid item xs={11}>
-            <Homepage/>
-          </Grid>
-        </Grid>
-
+    <Grid container spacing={0}>
+      <Grid item xs={false} md={1} lg={1}>
+        <SideBar className='SideBar'/>
+      </Grid>
+      <Grid item xs={12} md={11} lg={11}>
+        <Homepage/>
+      </Grid>
+    </Grid>
   );
 };
 
