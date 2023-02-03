fcc5fb9ce6de3e3f3736bcb7de81302b97d86d6b
Ivan Chernyak <ivanchernyak@Ivans-MacBook-Pro-2.local>
2023-02-03 11:53:15 -0800

    rudimentary tags for health

--- backend/sql/testdata.sql
+++ backend/sql/testdata.sql
@@ -1,7 +1,7 @@
 DELETE FROM recipes;
-INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData) VALUES ('Mushroom Poppers', '{"Baby Bella Mushrooms", "Cheese", "Jalepeno"}', '3', '/test.png');
-INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData) VALUES ('Chicken Parmesan', '{"Chicken Breast", "Parmesan", "Pasta", "Butter"}', '4', '/test.png');
-INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData) VALUES ('Cheeseburger', '{"Beef Patty", "Buns", "American Cheddar", "Lettuce", "Pickels", "Mayonaise", "Ketchup"}', '7', '/test.png');
+INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData, halal, kosher) VALUES ('Mushroom Poppers', '{"Baby Bella Mushrooms", "Cheese", "Jalepeno"}', '3', '/test.png', 'true', 'true');
+INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData, healthy, halal, kosher) VALUES ('Chicken Parmesan', '{"Chicken Breast", "Parmesan", "Pasta", "Butter"}', '4', '/test.png', 'true', 'true', 'true');
+INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData, kosher, halal) VALUES ('Cheeseburger', '{"Beef Patty", "Buns", "American Cheddar", "Lettuce", "Pickels", "Mayonaise", "Ketchup"}', '7', '/test.png', 'true', 'true');
 INSERT INTO recipes(dishname, ingredients, ingredientAm, imageData) VALUES ('Pepperoni Pizza', '{"Pizza Dough", "Pepperoni", "Mozzarella Cheese"}', '3', '/test.png');
 
 DELETE FROM users;
@@ -13,3 +13,8 @@ INSERT INTO users(username, passwrd) VALUES ('molly@books.com', '$2b$10$Y00XOZD/
 
 DELETE FROM meals;
 INSERT INTO meals(mealsid, recipeid, dayof) VALUES (1, 1, '2023-01-19');
+
+DELETE FROM substitutions;
+INSERT INTO substitutions(ingredient, tradeFor, veganAlternative) VALUES ('Whole Milk', '{"Skimmed Milk", "Fat Free Milk"}', '{"Almond Milk", "Soy Milk", "Oat Milk"}');
+INSERT INTO substitutions(ingredient, tradeFor, veganAlternative) VALUES ('Cheese', '{"Skimmed Milk", "Fat Free Milk"}', '{"Cashew Cheese", "Daiya Cheese"}');
+INSERT INTO substitutions(ingredient, tradeFor, veganAlternative) VALUES ('Beef', '{"Bison", "Lamb"}', '{"Tempeh"}');
