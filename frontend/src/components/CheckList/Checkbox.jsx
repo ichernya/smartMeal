69ab5156450a3d760911ea857de86095b430e407
Eric <meisericxie@gmail.com>
2023-02-08 16:08:51 -0800

    CheckList not working

--- frontend/src/components/CheckList/Checkbox.jsx
+++ frontend/src/components/CheckList/Checkbox.jsx
@@ -1,45 +1,79 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react';
+import Grid from '@mui/material/Grid';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
+import {useMeals} from '../MealContextProvider';
+// import children from './CheckChildren';
 import './Checkbox.css';
 
-// eslint-disable-next-line require-jsdoc
-export default function IndeterminateCheckbox() {
-  const [checked, setChecked] = React.useState([true, false]);
-
+const categories = [
+  'Protein',
+  'Vegetable',
+  'Dairy',
+  'Canned & Dried Produce',
+  'Baking',
+  'Herbs & Spices',
+  'Breads | Pasta | Grains',
+];
+const ingredientDict = {
+  'Protein': ['Chicken Breast'],
+  'Dairy': ['Parmesan', 'Butter', 'Cheese'],
+  'Vegetable': ['Baby Bella Mushrooms', 'Jalepeno'],
+  'Breads | Pasta | Grains': ['Pasta'],
+};
+/**
+ * @return {object}
+ */
+const IndeterminateCheckbox = () => {
+  const {meals} = useMeals();
+  const [needLoad, setNeedLoad] = useState(true);
+  const [categorieState, setCategorieState] = useState({
+    'Protein': {'checked': false, 'ingredients': []},
+    'Vegetable': {'checked': false, 'ingredients': []},
+    'Dairy': {'checked': false, 'ingredients': []},
+    'Canned & Dried roduce': {'checked': false, 'ingredients': []},
+    'Baking': {'checked': false, 'ingredients': []},
+    'Herbs & Spices': {'checked': false, 'ingredients': []},
+    'Breads | Pasta | Grains': {'checked': false, 'ingredients': []},
+  });
   const handleChange1 = (event) => {
-    setChecked([event.target.checked, event.target.checked]);
+    setCategorieState([event.target.checked, event.target.checked]);
   };
-
-  const children = (
-    <Box sx={{display: 'flex', flexDirection:
-      'column', ml: 3, gap: 1, mt: 1}}>
-      <FormControlLabel
-        label="Protein"
-        control={
-          <Checkbox
-            checked={checked[0] && checked[1]}
-            onChange={handleChange1}
-          />
-        }
-      />
-    </Box>
-  );
-
+  useEffect(() => {
+    categories.forEach((category) => {
+      if (categorieState[category] && ingredientDict[category] && needLoad) {
+        const newState = categorieState;
+        newState[category]['ingredients'] = (ingredientDict[category]);
+        setCategorieState(newState);
+      }
+    });
+  }, [categorieState, needLoad]);
+  console.log()
   return (
-    <div id="list">
-      <FormControlLabel
-        label='Groceries'
-        control={
-          <Checkbox
-            checked={checked[0] && checked[1]}
-            indeterminate={checked[0] !== checked[1]}
-            onChange={handleChange1}
-          />}
-      />
-      {children}
-    </div>
+    <Grid
+      container
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+    >
+      {categories.map((category) => (
+        <Grid item key={category}>
+          <FormControlLabel
+            label={category}
+            id={category}
+            control={
+              <Checkbox
+                id={category}
+                checked={categorieState[category]['checked']}
+                onChange={handleChange1}
+              />}
+          />
+        </Grid>
+      ))}
+    </Grid>
   );
-}
+};
+
+export default IndeterminateCheckbox;
