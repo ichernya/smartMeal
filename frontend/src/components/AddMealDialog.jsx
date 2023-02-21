4cb8ce7626e91c8c64fc9a32cf957f61f79798a2
AlbertoGaragnani <garagnanialberto@gmail.com>
2023-02-20 22:00:05 -0800

    Refactoring addMealDialog using Grid adn tother changes

--- frontend/src/components/AddMealDialog.jsx
+++ frontend/src/components/AddMealDialog.jsx
@@ -14,16 +14,19 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import Grid from '@mui/material/Grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddBoxIcon from '@mui/icons-material/AddBox';
+import AddPhotoAlternateRoundedIcon from
+  '@mui/icons-material/AddPhotoAlternateRounded';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import {Paper} from '@mui/material';
+import {Paper, Typography} from '@mui/material';
 import {TextField} from '@mui/material';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
+import './AddMealDialog.css';
 
 /**
  * @return {object}
@@ -39,11 +42,13 @@ export default function AddMealDialog() {
 
   // State
   const [open, setOpen] = React.useState(true);
+  const [recipeName, setRecipeName] = React.useState('');
   const [ingredients, setIngredients] = React.useState([]);
   const [inputName, setInputName] = React.useState('');
-  const [inputWeight, setInputWeight] = React.useState();
+  const [inputWeight, setInputWeight] = React.useState('');
   const [unit, setUnit] = React.useState('');
   const [system, setSystem] = React.useState('metric');
+  const [fileName, setFileName] = React.useState('Select Image');
   // States to handle input errors
   // Error on blank input name
   const [errorN, setErrorN] = React.useState(false);
@@ -52,6 +57,8 @@ export default function AddMealDialog() {
   // Error on duplicate input name
   const [errorSameN, setErrorSameN] = React.useState(false);
   const [errorU, setErrorU] = React.useState(false); // Error on blank unit
+  // Error on image selection
+  const [typographyColor, setTypographyColor] = React.useState('black');
 
   /**
    * Return an array with only the name of ingredients
@@ -128,11 +135,29 @@ export default function AddMealDialog() {
     setUnit('');
   };
 
+  const changeRecipeName = (event) => {
+    setRecipeName(event.target.value);
+  };
+
   // Remove an ingredient from the list
   const removeIngredient = (name) => {
     const newIngredients = [...ingredients]
       .filter((ingredient) => ingredient.name !== name);
     setIngredients(newIngredients);
+  };
+
+  const handleCapture = (event) => {
+    /* setSelectedFile(URL.createObjectURL(event.target.files[0])); */
+    const imageName = event.target.files[0].name;
+    if (imageName.toUpperCase().endsWith('.JPEG') ||
+      imageName.toUpperCase().endsWith('.JPG') ||
+      imageName.toUpperCase().endsWith('.PNG')) {
+      setFileName(imageName);
+      setTypographyColor('black');
+    } else {
+      setFileName('Format accepted: jpeg | jpg | png');
+      setTypographyColor('red');
+    }
   };
 
   return (
@@ -157,111 +182,148 @@ export default function AddMealDialog() {
             You can add your own recipe adding a title,
             ingredients and an optional URL image.
           </DialogContentText>
-          <TextField
-            sx={{width: 300}}
-            label="Name of Recipe"
-            variant='outlined'
-            required
-            margin='normal'
-          />
-          <FormControl sx={{mt: 2, ml: 4}}>
-            <FormLabel id="radio-label">System</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="radio-label"
-              name="row-radio-buttons-group"
-              value={system}
-              onChange={changeSystem}
-            >
-              <FormControlLabel value="metric" control={<Radio />}
-                label="Metric" />
-              <FormControlLabel value="US" control={<Radio />} label="US" />
-            </RadioGroup>
-          </FormControl>
-          <TextField
-            fullWidth
-            label="Image"
-            variant='outlined'
-            margin='normal'
-          />
-          <TextField
-            sx={{width: 300}}
-            label="Ingredient"
-            variant='outlined'
-            margin='normal'
-            value={inputName}
-            error={errorN || errorSameN}
-            helperText={errorSameN ? 'You already added this ingredient' : ''}
-            onChange={(event) => setInputName(event.target.value)}
-          />
-          <FormControl sx={{m: 2, width: 80}} variant="outlined">
-            <OutlinedInput
-              id="outlined-weight"
-              value={inputWeight}
-              aria-describedby="outlined-weight-helper-text"
-              inputProps={{
-                'aria-label': 'weight',
-              }}
-              error={errorW}
-              onChange={(event) => setInputWeight(event.target.value)}
-            />
-            <FormHelperText id="outlined-weight-helper-text">
-              Quantitiy
-            </FormHelperText>
-          </FormControl>
-          <FormControl>
-            <Select sx={{mt: 2, maxWidth: 70}}
-              aria-describedby="select-text"
-              error={errorU}
-              value={unit}
-              onChange={changeUnit}
-            >
-              {(system === 'metric' ? metricUnits : USUnits).map((unit) => (
-                <MenuItem value={unit}>{unit}</MenuItem>
-              ))}
-            </Select>
-            <FormHelperText id="select-text">Unit</FormHelperText>
-          </FormControl>
-          <IconButton
-            variant="outlined"
-            sx={{mt: 2, ml: 1}}
-            size="large"
-            onClick={addIngredient}
-          >
-            <AddBoxIcon fontSize='large'/>
-          </IconButton>
-          {/* The paper show the ingredients list*/}
-          <Paper style={{maxHeight: 200, overflow: 'auto'}}>
-            <List>
-              {ingredients.map((ingredient) => (
-                <ListItem alignItems='center'>
-                  {/* The Grid is used the components of a sigle list item */}
-                  <Grid container spacing={2} component="main" direction="row">
-                    <Grid item sm={7} overflow='auto'>
-                      <ListItemText>
-                        {ingredient.name}
-                      </ListItemText>
-                    </Grid>
-                    <Grid item sm={3}>
-                      <ListItemText>
-                        {ingredient.quantity}{ingredient.unit}
-                      </ListItemText>
-                    </Grid>
-                    <Grid item sm={2} alignItems='right' sx={{
-                      float: 'right',
-                    }}>
-                      <ListItemButton
-                        alignItems='center'
-                        variant="outlined"
-                        size="large"
-                        onClick={() => removeIngredient(ingredient.name)}
-                      ><DeleteIcon/></ListItemButton>
-                    </Grid>
-                  </Grid>
-                </ListItem>
-              ))}
-            </List>
-          </Paper>
+          <Grid container component="main" direction="row">
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Name of Recipe"
+                variant='outlined'
+                required
+                margin='normal'
+                onChange={changeRecipeName}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <FormControl sx={{mt: 2, ml: 2}}>
+                <FormLabel id="radio-label">System</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="radio-label"
+                  name="row-radio-buttons-group"
+                  value={system}
+                  onChange={changeSystem}
+                >
+                  <FormControlLabel value="metric" control={<Radio />}
+                    label="Metric" />
+                  <FormControlLabel value="US" control={<Radio />} label="US" />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            <Grid item xs={10}>
+              <div>
+              <Paper className='paper'>
+                <Typography sx={{m: 2, overflow: 'scroll'}}
+                  color={typographyColor}>
+                  {fileName}
+                </Typography>
+              </Paper>
+              </div>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton
+                component="label"
+                variant="outlined"
+                sx={{mt: 1}}
+              >
+                <AddPhotoAlternateRoundedIcon fontSize='large'/>
+                <input
+                  type="file"
+                  hidden
+                  onChange={handleCapture}
+                />
+              </IconButton>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                fullWidth
+                label="Ingredient"
+                variant='outlined'
+                margin='normal'
+                value={inputName}
+                error={errorN || errorSameN}
+                helperText={errorSameN ?
+                  'You already added this ingredient' : ''}
+                onChange={(event) => setInputName(event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={10} md={4}>
+              <FormControl className='quantity'
+                sx={{m: 2, maxWidth: 80}}
+                variant="outlined">
+                <OutlinedInput
+                  id="outlined-weight"
+                  value={inputWeight}
+                  aria-describedby="outlined-weight-helper-text"
+                  inputProps={{
+                    'aria-label': 'weight',
+                  }}
+                  error={errorW}
+                  onChange={(event) => setInputWeight(event.target.value)}
+                />
+                <FormHelperText id="outlined-weight-helper-text">
+                  Quantitiy
+                </FormHelperText>
+              </FormControl>
+              <FormControl>
+                <Select sx={{mt: 2}}
+                  aria-describedby="select-text"
+                  error={errorU}
+                  value={unit}
+                  onChange={changeUnit}
+                >
+                  {(system === 'metric' ? metricUnits : USUnits).map((unit) => (
+                    <MenuItem value={unit}>{unit}</MenuItem>
+                  ))}
+                </Select>
+                <FormHelperText id="select-text">Unit</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item xs={2}>
+              <IconButton
+                variant="outlined"
+                sx={{mt: 2}}
+                size="large"
+                onClick={addIngredient}
+              >
+                <AddBoxIcon fontSize='large'/>
+              </IconButton>
+            </Grid>
+            <Grid item xs={12}>
+            {/* The paper show the ingredients list*/}
+              <Paper style={{maxHeight: 200, overflow: 'auto'}}>
+                <List>
+                  {ingredients.map((ingredient) => (
+                    <ListItem alignItems='center'>
+            {/* The Grid is used the components of a sigle list item */}
+                      <Grid container spacing={2} component="main"
+                        direction="row">
+                        <Grid item xs={7} overflow='auto'>
+                          <ListItemText>
+                            {ingredient.name}
+                          </ListItemText>
+                        </Grid>
+                        <Grid item xs={3}>
+                          <ListItemText>
+                            {ingredient.quantity}{ingredient.unit}
+                          </ListItemText>
+                        </Grid>
+                        <Grid item xs={2} alignItems='right' sx={{
+                          float: 'right',
+                        }}>
+                          <ListItemButton
+                            alignItems='center'
+                            variant="outlined"
+                            size="large"
+                            onClick={() => removeIngredient(ingredient.name)}
+                          ><DeleteIcon/></ListItemButton>
+                        </Grid>
+                      </Grid>
+                    </ListItem>
+                  ))}
+                </List>
+              </Paper>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
           <Button>Add recipe</Button>
