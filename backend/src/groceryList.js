bc08bdd922445936f5dbc223e2a16c735e6dbad8
ichernya <ichernya@ucsc.edu>
2023-03-12 15:07:02 -0700

    done

---
+++ backend/src/groceryList.js
@@ -0,0 +1,46 @@
+const {Pool} = require('pg');
+
+const user = "postgres";
+const host = "localhost";
+const database = "dev";
+const password = "postgres";
+const port = "5432";
+
+const pool = new Pool({
+  host: host,
+  port: port,
+  database: database,
+  user: user,
+  password: password,
+});
+
+const updatePutQuery = async (firstDay, mealsid, category, ingredient) => {
+    const update = `
+    UPDATE checklistUser
+    SET checklist = jsonb_set(checklist, $3, 'true', true)
+    WHERE mealsid = $2 AND firstDay = $1;`
+    const query = {
+        text: update,
+        values: [firstDay, mealsid, `{${category},"ingredients","${ingredient}","checked"}`]
+    }
+    await pool.query(query);
+
+    
+    const result = await pool.query(
+      `UPDATE checklistUser
+       SET checklist = jsonb_set(checklist, '{${category},amountChecked}', ((checklist->'${category}'->>'amountChecked')::integer + 1)::text::jsonb)      
+       WHERE mealsid = $2 AND firstDay = $1
+       RETURNING checklist`,
+      [firstDay, mealsid]
+    );
+
+    const updatedList = result.rows[0].checklist;
+
+    return updatedList;
+    
+}
+exports.updateAsChecked = async (req, res) => {
+    
+    const update = await updatePutQuery(req.body.firstDay, req.body.mealsid, req.body.category, req.body.ingredient);
+    res.status(201).json(update)
+}
