14b4422d868f893cc77c4caa3296598835a209c5
Eric <meisericxie@gmail.com>
2023-03-13 22:29:45 -0700

    checklist

--- frontend/src/components/GenerateList.jsx
+++ frontend/src/components/GenerateList.jsx
@@ -1,12 +1,22 @@
 const createList = (setIngredientList, startWeek) => {
-  const [month, day, year] = startWeek.toLocaleDateString().split('/');
-  const startIso = `${year}-${month}-${day}`;
+  // calculates the start and end of the week
+  const currentDay = new Date();
+  const dateOffset = currentDay.getDay();
+  startWeek.setDate(currentDay.getDate() - dateOffset);
+  let [month, day, year] = startWeek.toLocaleDateString().split('/');
 
+  if (parseInt(month) < 10) {
+    month = '0' + month;
+  }
+  if (parseInt(day) < 10) {
+    day = '0' + day;
+  }
+  const start = `${year}-${month}-${day}`;
   const item = localStorage.getItem('user');
   const person = JSON.parse(item);
   const userId = person.userid;
   const bearerToken = person ? person.accessToken : '';
-  fetch(`http://localhost:3010/v0/groceryList?mealsid=${userId}&firstDay=${startIso}`, {
+  fetch(`http://localhost:3010/v0/groceryList?mealsid=${userId}&firstDay=${start}`, {
     method: 'get',
     headers: new Headers({
       'Authorization': `Bearer ${bearerToken}`,
